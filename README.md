540ede88371698a1f346a2faf7a57be44d611e1d
Gannio <thegreatgannio@gmail.com>
2022-02-18 00:20:19 -0700

    Readme, added sub-folders.

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-# Arc Randomizer
+# ARClassic: Classic Mode Selection Tool
 ## Prerequisites
 - [ARCropolis 1.1.3 or higher](https://github.com/Raytwo/ARCropolis/releases/latest)
-- [Skyline (Should come with ARCropolis)](https://github.com/skyline-dev/skyline/releases/tag/beta)
+- [Skyline](https://github.com/skyline-dev/skyline/releases/tag/beta)
+- [ARCExplorer](https://github.com/ScanMountGoat/ArcExplorer) (For grabbing Classic Mode Routes from the main game).
+
 
 ## Setup
-Download the latest version of this plugin and put it in:
-`sd:/atmosphere/contents/01006A800016E000/romfs/skyline/plugins`
+1. Download the latest version from the Releases tab, then install to the root of your SD card.
+2. Using ARCExplorer, extract all files named "standard_route_[NameOfFighter].prc" from "ui/param/standard" and "ui/param_patch_standard" and move them to "/atmosphere/contents/01006A800016E000/romfs/ClassicModeSelector/ClassicDefaults/" in your SD card.
+3. (Optional) For any additional Classic Mode mods you have, create a folder (or folders, if you have many mods that can be seperated) within "/atmosphere/contents/01006A800016E000/romfs/ClassicModeSelector/" to store them in. This will allow you to choose them seperately from the default ones. Note they do not have to be the name of a character's route, so you can rename them however you see fit. It is recommended these folders are appending with 'Custom' or some other word that comes after Classic, in order to keep default classic modes at ID 0.
 
-## Usage
-Create a folder called `Randomizer` in the following path:
-
-`sd:/atmosphere/contents/01006A800016E000/romfs/`
-
-Then replicate the ARC path for the file you want to randomize.
-
-For Example:
-
-Stream - `sd:/atmosphere/contents/01006A800016E000/romfs/Randomizer/stream;/sound/bgm/bgm_crs2_02_senjyou.nus3audio/*.nus3audios`
-(Randomizes song anytime the song "Battlefield" is played)
-
-Arc - `sd:/atmosphere/contents/01006A800016E000/romfs/Randomizer/ui/message/msg_bgm.msbt/*.msbt`
-(Randomizes msbt file anytime msg_bgm.msbt is loaded)
+## In-Game Usage
+1. Select a character from the Classic Mode menu. THis will prompt a keyboard instruction.
+2. (This step only applies if you followed #3 above) Select the ID number corresponding to your folder.
+3. Type the ID number corresponding to the file you want to use (See DefaultRoutes.csv for numbers corresponding to the defaults). Alternatively, type 'R' or 'r' (Not the button R, the letter on the keyboard), to randomly choose from the folder.
+This route will stay until it's unloaded from memory, so to change the route for a character after already choosing, choose another character, then go back after entering the difficulty screen.
+The route will also not change name to reflect the new route (this is because the names are stored seperately from the actual routes).
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
-#![feature(proc_macro_hygiene)]
+#![feature(proc_macro_hygiene, new_uninit)]
 
 
 #[macro_use]
 extern crate lazy_static;
+use crate::keyboard::ShowKeyboardArg;
 
 use std::{fs, io::{Error, ErrorKind, Result}, path::{Path, PathBuf}, collections::HashMap, sync::Mutex};
 use rand::Rng;
 use walkdir::WalkDir;
 use arcropolis_api::*;
 
-const RANDOMIZE_PATH: &str = "rom:/Randomizer/";
+mod keyboard;
+
+const RANDOMIZE_PATH: &str = "rom:/ClassicModeSelector_FilesToCatch/";
+const FILECHOICE_PATH: &str = "rom:/ClassicModeSelector";
 
 lazy_static! {
     static ref FILE_HOLDER: Mutex<HashMap<u64, PathBuf>> = {
@@ -18,35 +22,117 @@ lazy_static! {
     };    
 }
 
-pub fn random_file_select(directory: &Path) -> Result<String>{
+pub fn random_file_select(directory: &Path) -> Result<Vec<String>>{
     let mut rng = rand::thread_rng();
 
-    let mut files = HashMap::new();
 
-    for entry in fs::read_dir(directory)? {
-        let entry = entry?;
-        let path = entry.path();
-        if !&path.is_dir() {
-            files.insert(files.len(), format!("{}", path.display()));
-        }
-    }
 
-    let count = files.len();
 
-    if count <= 0 {
-        return Err(Error::new(ErrorKind::Other, "No Files Found!"))
-    }
-    
-    let random_result = rng.gen_range(0..count);
 
-    Ok(files.get(&random_result).unwrap().to_string())
+
+	
+	let mut folders = HashMap::new();
+	for entry in fs::read_dir(FILECHOICE_PATH)?
+	{
+		
+		let entry = entry?;
+		let path = entry.path();
+		if path.is_dir() {
+			folders.insert(folders.len(), format!("{}", path.display()));
+		}
+		
+	}
+	let mut folder_choice = 0;
+	if folders.len() > 1
+	{
+		println!("{}","Many Folders.");
+		let folder_text = format!("Enter Folder ID (0-{})", (folders.len()-1).to_string());
+		folder_choice = (ShowKeyboardArg::new().header_text(&folder_text)
+						.show().unwrap_or("0".to_string())).parse::<usize>().unwrap();
+	}
+	else if folders.len() <= 0
+	{
+		println!("{}","No folders");
+		//println!("{}","No folders");
+		return Err(Error::new(ErrorKind::Other, "No Folders Found! Please add some to ClassicModeSelector!"))
+	}
+
+	if folder_choice >= folders.len()
+	{
+		folder_choice = 0;
+	}
+
+	println!("{}",folder_choice);
+	println!("{}",folders.get(&folder_choice).unwrap().to_string());
+
+
+	let mut files = HashMap::new();
+	//println!("{}",directory.display());
+	
+	
+	
+	
+	
+	
+	
+	for entry in fs::read_dir(/*directory*/folders.get(&folder_choice).unwrap().to_string())? {
+		let entry = entry?;
+		let path = entry.path();
+		if !&path.is_dir() {
+			files.insert(files.len(), format!("{}", path.display()));
+		}
+	}
+
+	let count = files.len();
+
+	if count <= 0 {
+		return Err(Error::new(ErrorKind::Other, "No Files Found!"))
+	}
+	
+	
+	let display = format!("Enter Classic Mode ID (0-{})", (count-1).to_string());
+	
+	
+	let mut user_input = ShowKeyboardArg::new().header_text(&display).show().unwrap_or("0".to_string());
+	
+	
+	if user_input == "R" || user_input == "r"
+	{
+		println!("{}","Random");
+		user_input = rng.gen_range(0..count).to_string();
+	}
+	
+	let mut random_result = user_input.parse::<usize>().unwrap();
+	//let random_result = rng.gen_range(0..count);
+	
+	if random_result >= files.len()
+	{
+		//return Err(Error::new(ErrorKind::Other, "File chosen outside range!"))
+		random_result = 0;
+	}
+	
+	println!("{}",files.get(&random_result).unwrap().to_string());
+	Ok(vec![files.get(&random_result).unwrap().to_string(),directory.display().to_string()])
 }
 
 #[arc_callback]
 fn arc_file_callback(hash: u64, data: &mut [u8]) -> Option<usize>{
+	
+	//let ogFile = ;
+	
     match random_file_select(FILE_HOLDER.lock().unwrap().get(&hash).unwrap()){
-        Ok(s) => {
+        Ok(col) => {
+			let s = &col[0];
+			let d = &col[1];
+			println!("{}",s.to_string());
+			println!("{}",d.to_string());
+			
             let file = fs::read(s).unwrap();
+			//let file2 = fs::read(d).unwrap();
+			
+			println!("{:?}",..file.len());
+			//println!("{:?}",..file.len());
+			//println!("{:?}",..file2.len());
             
             // Shoutouts to Genwald
             data[..file.len()].copy_from_slice(&file);
@@ -57,6 +143,7 @@ fn arc_file_callback(hash: u64, data: &mut [u8]) -> Option<usize>{
     }
 }
 
+/*
 #[stream_callback]
 fn stream_file_callback(hash: u64) -> Option<String>{    
     match random_file_select(FILE_HOLDER.lock().unwrap().get(&hash).unwrap()){
@@ -64,12 +151,28 @@ fn stream_file_callback(hash: u64) -> Option<String>{
         Err(_err) => None
     }
 }
+*/
+
 
 fn get_biggest_size_from_path(path: &Path) -> usize{
     let mut biggest_size: usize = 0;
 
     for entry in fs::read_dir(path).unwrap() {
-        let size = entry.unwrap().metadata().unwrap().len() as usize;
+		
+		let size;
+		let entry_inner = entry.unwrap();
+		
+		//let entry = entry?;
+		let path = entry_inner.path();
+		if path.is_dir() {
+			size = get_biggest_size_from_path(&path);
+		}
+		else
+		{
+			size = entry_inner.metadata().unwrap().len() as usize;
+		
+			
+		}
         if size > biggest_size {
             biggest_size = size;
         }
@@ -95,10 +198,11 @@ pub fn main() {
             
             FILE_HOLDER.lock().unwrap().insert(hash.as_u64(), entry.path().to_path_buf());
             
-            if path.contains("stream"){
-                stream_file_callback::install(hash);
+            if path.contains("stream"){//None of these should be stream files.
+                //stream_file_callback::install(hash);
             }else{
-                arc_file_callback::install(hash, get_biggest_size_from_path(&entry.path()));
+				let path = Path::new(FILECHOICE_PATH);
+                arc_file_callback::install(hash, get_biggest_size_from_path(/*&entry.path()*/path));
             }
 
         }
